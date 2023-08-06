93499ba1b9ce5f0117d60caa4ab2226ad67e9150
André Luiz Barros <77760155+andre55255@users.noreply.github.com>
2023-08-06 01:30:24 -0300

    Feat - Added public layout theme toggler button icon

--- src/components/public-layout/header/index.tsx
+++ src/components/public-layout/header/index.tsx
@@ -2,13 +2,22 @@ import { useTheme } from "../../../hooks/use-theme";
 import { ContainerHeader, ImageHeaderLogo } from "./styled";
 import logoLight from "../../../assets/logo-ad-cutted.png";
 import logoDark from "../../../assets/logo-ad-dark-cutted.png";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 export default function HeaderPublicLayout() {
-    const { theme } = useTheme();
+    const { theme, themeToggler } = useTheme();
     
+    const handleTheme = () => {
+        themeToggler();
+        window.location.reload();
+    }
+
     return (
         <ContainerHeader>
             <ImageHeaderLogo src={theme === "dark" ? logoDark : logoLight} alt="logo" />
+            {theme === "dark" ?
+             <FaSun onClick={handleTheme} /> :
+             <FaMoon onClick={handleTheme} />}
         </ContainerHeader>
     );
 }
--- src/components/public-layout/header/styled.tsx
+++ src/components/public-layout/header/styled.tsx
@@ -3,10 +3,22 @@ import { StyledComponentProps } from "../../../types/styled-component-props";
 
 export const ContainerHeader = styled.header<StyledComponentProps>`
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
+    flex-wrap: wrap;
     padding: 1rem 0;
     height: 20vh;
+
+    svg {
+        color: ${props => props.theme.colorLabelForm};
+        font-size: 1.5rem;
+        transition: all .5s;
+        cursor: pointer;
+
+        &:hover {
+            font-size: 1.7rem;
+        }
+    }
 `;
 
 export const ImageHeaderLogo = styled.img<StyledComponentProps>`
